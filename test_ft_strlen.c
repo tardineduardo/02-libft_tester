29309ff1e2b22646a0b4c6db214d8aa414eab6ea
Eduardo Ribeiro Tardin Costa <eduribei@c4a9c2.42berlin.de>
2024-04-23 21:45:36 +0200

    .

--- test_ft_strlen.c
+++ test_ft_strlen.c
@@ -6,13 +6,13 @@
 /*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 18:22:00 by eduribei          #+#    #+#             */
-/*   Updated: 2024/04/23 21:38:23 by eduribei         ###   ########.fr       */
+/*   Updated: 2024/04/23 21:45:23 by eduribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_strlen(int c);
+int	ft_strlen(char *c);
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
