a66b4de4ffdbb0c59366314fce6b0ca9a3aae9b9
Tadeu Duarte Fake <tadeu.duarte.fake@gmail.com>
2024-04-24 19:28:28 +0200

    Atoi is ready

--- tests/test_ft_atoi.c
+++ tests/test_ft_atoi.c
@@ -1,53 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_ft_isdigit.c                                  :+:      :+:    :+:   */
+/*   test_ft_atoi.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 18:22:00 by eduribei          #+#    #+#             */
-/*   Updated: 2024/04/24 10:28:10 by eduribei         ###   ########.fr       */
+/*   Updated: 2024/04/24 17:15:36 by eduribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int	ft_atoi(const char *nptr);
+
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+void	test_ft_atoi(char *nptr, char *comment)
+{
+	int	result = ft_atoi(nptr);
+	int	expected = atoi(nptr);
+
+	if (result == expected) 
+		printf(ANSI_COLOR_GREEN "[[[PASS]]] " ANSI_COLOR_RESET);
+	else
+		printf(ANSI_COLOR_RED "[[[FAIL]]] " ANSI_COLOR_RESET);
+
+	printf("ft_strlen > Input: <\"%s\"> (%s) | Expected: %d | Output: %d \n", nptr, comment, expected, result);
+
+}
 
 int	main(void)
 {
-    printf("\033[1;34mThis test is not ready yet.\033[0m\n\n");
+	printf(">>>>> TESTING FT_ATOI\n");
+	test_ft_atoi("Twinkle twinkle little star...", "Normal string");
+	test_ft_atoi("", "Empty string");
+	test_ft_atoi("1", "String with one numeric char");
+	test_ft_atoi("+35", "String with plus sign");
+	test_ft_atoi("  ", "String with only spaces");
+	test_ft_atoi("-9384", "String with minus sign");
+	test_ft_atoi("   84", "String with trailing spaces at the beginning");
+	test_ft_atoi("84   ", "String with trailing spaces at the end");
+	test_ft_atoi(" 84  ", "String with trailing spaces at both ends");
+
+	printf("\n");
+
+	return (0);
 }
-
-
-
-// int	ft_strlen(char *c);
-
-// #define ANSI_COLOR_RED     "\x1b[31m"
-// #define ANSI_COLOR_GREEN   "\x1b[32m"
-// #define ANSI_COLOR_RESET   "\x1b[0m"
-
-// void	test_ft_strlen(char* c, int expected, char *comment)
-// {
-// 	int	result = ft_strlen(c);
-
-// 	if (result == expected) 
-// 		printf(ANSI_COLOR_GREEN "[[[PASS]]] " ANSI_COLOR_RESET);
-// 	else
-// 		printf(ANSI_COLOR_RED "[[[FAIL]]] " ANSI_COLOR_RESET);
-
-// 	printf("ft_strlen > Input: <\"%s\"> (%s) | Expected: %d | Output: %d \n", c, comment, expected, result);
-
-// }
-
-// int	main(void)
-// {
-// 	printf(">>>>> TESTING FT_STRLEN\n");
-// 	test_ft_strlen("batatinha quando nasce...", 25, "Normal string");
-// 	test_ft_strlen("", 0, "Empty string");
-// 	test_ft_strlen("1", 1, "String with one char");
-//  test_ft_strlen("  ", 2, "String with only spaces");
-//  test_ft_strlen("hello\0world", 5, "String with null terminator in the middle");
-//	printf("\n");
-
-
-// 	return (0);
-// }
