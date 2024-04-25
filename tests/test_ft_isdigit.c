eb14dedc1516f80af0327ed5f6aae0e006c90d66
Bruna Chaves Batista Sousa <brchaves@c3a4c9.42berlin.de>
2024-04-25 10:31:23 +0200

    isdigit test

--- tests/test_ft_isdigit.c
+++ tests/test_ft_isdigit.c
@@ -3,17 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   test_ft_isdigit.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: brchaves <brchaves@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 18:22:00 by eduribei          #+#    #+#             */
-/*   Updated: 2024/04/24 10:28:10 by eduribei         ###   ########.fr       */
+/*   Updated: 2024/04/25 10:30:21 by brchaves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <ctype.h>
+
+int	ft_isdigit(int c);
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+void	test_ft_isdigit(int c, char *comment)
+{
+	int	result = ft_isdigit(c);
+	int	expected = isdigit(c);
+
+	if ((result == 0) && (expected == 0)) 
+		printf(ANSI_COLOR_GREEN "[[[PASS]]] " ANSI_COLOR_RESET);
+	else if ((result != 0) && (expected != 0))
+		printf(ANSI_COLOR_GREEN "[[[PASS]]] " ANSI_COLOR_RESET);
+	else if ((result == 0 && expected != 0) || (result != 0 && expected == 0))
+		printf(ANSI_COLOR_RED "[[[FAIL]]] " ANSI_COLOR_RESET);
+
+	printf("ft_isdigit > Input: < %c > (%s) | Expected: %d | Output: %d \n", c, comment, expected, result);
+
+}
 
 int	main(void)
 {
-    printf("\033[1;34mThis test is not ready yet.\033[0m\n\n");
-}
+	printf(">>>>> TESTING FT_ISDIGIT\n");
 
+	test_ft_isdigit(" ", "space");
+	test_ft_isdigit("\0", "null");
+	test_ft_isdigit("a", "lowercase letter");
+    test_ft_isdigit("9", "digit");
+	printf("\n");
+
+	return (0);
+}
