5f8717b9af03108fc8d61a19e0b300525dd9f805
Eduardo Ribeiro Tardin Costa <eduribei@c4a9c2.42berlin.de>
2024-04-27 14:52:38 +0200

    Lots of changes

--- tests/test_ft_lstadd_back.c
+++ tests/test_ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 14:21:39 by eduribei          #+#    #+#             */
-/*   Updated: 2024/04/27 14:49:46 by eduribei         ###   ########.fr       */
+/*   Updated: 2024/04/27 14:52:03 by eduribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 int	main(void)
 {
-    printf(COLOR_BLUE "test_ft_lstadd_back - This test is not ready yet. " COLOR_RESET);
+	printf(COLOR_BLUE "test_ft_lstadd_back - This test is not ready yet. " COLOR_RESET);
 }
--- tests/test_ft_putstr_fd.c
+++ tests/test_ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 18:22:00 by eduribei          #+#    #+#             */
-/*   Updated: 2024/04/27 14:49:17 by eduribei         ###   ########.fr       */
+/*   Updated: 2024/04/27 14:52:03 by eduribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int	main(void)
 {
-    printf(COLOR_BLUE "test_ft_putstr_fd - This test is not ready yet. " COLOR_RESET);
+	printf(COLOR_BLUE "test_ft_putstr_fd - This test is not ready yet. " COLOR_RESET);
 }
+
 
 
