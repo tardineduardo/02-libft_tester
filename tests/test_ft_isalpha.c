75be2b2b999735c1107dd953c1cce051b1742792
Eduardo Ribeiro Tardin Costa <eduribei@c4a9c2.42berlin.de>
2024-04-28 17:57:15 +0200

    .

--- tests/test_ft_isalpha.c
+++ tests/test_ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 18:22:00 by eduribei          #+#    #+#             */
-/*   Updated: 2024/04/28 17:51:19 by eduribei         ###   ########.fr       */
+/*   Updated: 2024/04/28 17:56:46 by eduribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,38 +50,38 @@ int	main(void)
 
 	tests[0].input = -1;
     tests[0].comment = "smaller than ascii range";
-    tests[0].result = ft_isalnum(tests[0].input);
-    tests[0].expected = isalnum(tests[0].input);
+    tests[0].result = ft_isalpha(tests[0].input);
+    tests[0].expected = isalpha(tests[0].input);
 
     tests[1].input = 0;
     tests[1].comment = "zero";
-    tests[1].result = ft_isalnum(tests[1].input);
-    tests[1].expected = isalnum(tests[1].input);
+    tests[1].result = ft_isalpha(tests[1].input);
+    tests[1].expected = isalpha(tests[1].input);
 
     tests[2].input = 127;
     tests[2].comment = "bigger ascii number";
-    tests[2].result = ft_isalnum(tests[2].input);
-    tests[2].expected = isalnum(tests[2].input);
+    tests[2].result = ft_isalpha(tests[2].input);
+    tests[2].expected = isalpha(tests[2].input);
 
     tests[3].input = 178;
     tests[3].comment = "non-standard ASCII value";
-    tests[3].result = ft_isalnum(tests[3].input);
-    tests[3].expected = isalnum(tests[3].input);
+    tests[3].result = ft_isalpha(tests[3].input);
+    tests[3].expected = isalpha(tests[3].input);
 
     tests[4].input = 'a';
     tests[4].comment = "lowercase letter";
-    tests[4].result = ft_isalnum(tests[4].input);
-    tests[4].expected = isalnum(tests[4].input);
+    tests[4].result = ft_isalpha(tests[4].input);
+    tests[4].expected = isalpha(tests[4].input);
 
     tests[5].input = 'Z';
     tests[5].comment = "uppercase letter";
-    tests[5].result = ft_isalnum(tests[5].input);
-    tests[5].expected = isalnum(tests[5].input);
+    tests[5].result = ft_isalpha(tests[5].input);
+    tests[5].expected = isalpha(tests[5].input);
 
     tests[6].input = '5';
     tests[6].comment = "number";
-    tests[6].result = ft_isalnum(tests[6].input);
-    tests[6].expected = isalnum(tests[6].input);
+    tests[6].result = ft_isalpha(tests[6].input);
+    tests[6].expected = isalpha(tests[6].input);
 	
 	printf(">>>>> TESTING FT_isalpha\n");
 
