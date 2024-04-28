4975f22b427a27175f36d987768b5d89172189b8
Eduardo Ribeiro Tardin Costa <eduribei@c4a9c2.42berlin.de>
2024-04-28 17:58:05 +0200

    .

--- tests/test_ft_toupper.c
+++ tests/test_ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 18:22:00 by eduribei          #+#    #+#             */
-/*   Updated: 2024/04/28 16:37:54 by eduribei         ###   ########.fr       */
+/*   Updated: 2024/04/28 17:57:57 by eduribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ void	test_ft_toupper(int c, char *comment)
 	else
 	{
 		printf(COLOR_RED "[[[FAIL]]] " COLOR_RESET);
-		(*counter)++;
 	}
 	printf("Input: <\"%d\"> (%s) | Expected: %d | Output: %d \n", c, comment, expected, result);
 
